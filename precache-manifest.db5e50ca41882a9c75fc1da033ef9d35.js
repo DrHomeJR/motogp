1d6049d20d3e3fceeb3867ca7d6eecb8e873fb9f
DrHomeJR <dev.m4r10@yahoo.com>
2019-07-12 09:23:59 +0200

    Updates

---
+++ precache-manifest.db5e50ca41882a9c75fc1da033ef9d35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9331b2a8fcf704e8a932a3c51847cf",
+    "url": "/motogp/index.html"
+  },
+  {
+    "revision": "e978dd2314783a6f1021",
+    "url": "/motogp/static/css/main.489a69cc.chunk.css"
+  },
+  {
+    "revision": "36195ce9c5eb964062d4",
+    "url": "/motogp/static/js/2.36a9cd08.chunk.js"
+  },
+  {
+    "revision": "e978dd2314783a6f1021",
+    "url": "/motogp/static/js/main.44acb10e.chunk.js"
+  },
+  {
+    "revision": "e7c163a2234fb742ee83",
+    "url": "/motogp/static/js/runtime~main.a2a29505.js"
+  }
+]);
